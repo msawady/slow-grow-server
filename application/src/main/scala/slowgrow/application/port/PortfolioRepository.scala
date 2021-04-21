d300c4fb60384a172fe2826deb3d2a1911b5ca17
msawady <msawady79@gmail.com>
2021-04-21 10:44:25 +0900

    fix

--- application/src/main/scala/slowgrow/application/port/PortfolioRepository.scala
+++ application/src/main/scala/slowgrow/application/port/PortfolioRepository.scala
@@ -1,12 +1,11 @@
 package slowgrow.application.port
 
 import cats.effect.IO
-import slowgrow.domain.portfolio.{Portfolio, Trade}
+import slowgrow.domain.portfolio.Portfolio
 import slowgrow.domain.values.InvestorId
 
 trait PortfolioRepository {
 
   def findBy(investorId: InvestorId, portfolioName: Portfolio.Name): IO[Either[DataAccessException, Portfolio]]
 
-  def appendTrade(portfolioId: Portfolio.Id, tradeId: Trade.Id): IO[Either[DataCreateException, Unit]]
 }
